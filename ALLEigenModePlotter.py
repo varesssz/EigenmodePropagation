f402d306ff0e09baa934dc2910bb14f194b3aef7
Daniel Varga <vargad9797@gmail.com>
2023-11-29 23:00:44 +0100

    Delete PW set calculation from ALLEigenModePlotter.py
    PW set calculation was unnecessary, only the transfer matrix is
    needed. Which is now can be selected, what type of model should it
    originate from.
    
    Also set back the ranges to default.

--- ALLEigenModePlotter.py
+++ ALLEigenModePlotter.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pickle
 from MyPlotlyFigure import MyPlotlyFigure
-from PlaneWaveSet import PlaneWaveSet
 
 
 if __name__ == '__main__':
 
+    # pw_set_model = "plane_wave"
+    # pw_set_model = "points_along_line"
+    # pw_set_model = "points_along_circle"
+    pw_set_model = "points_as_phased_array"
+
     e_field_all_eigen = np.genfromtxt(
-        fname="MATLAB/data/structC_w120_x5605_k801/0deg_to_180deg_excitation/eigenmode_y=-1_5_all.txt",
+        fname="MATLAB/output/all_eigenmodes_by_%s.txt" % pw_set_model,
         dtype=np.complex64,
         delimiter=",",
     )
@@ -19,31 +23,18 @@ if __name__ == '__main__':
         z=np.abs(e_field_all_eigen),
         colorbar_title_text="E_z absolute value [V/m]",
         colorbar_title_side="right",
-        # colorscale="jet",
-        # zmid=0,
-        # zmax=10,
-        # zmin=-10,
     )
     fig.update_layout(
         xaxis_title="x [m]",
         # yaxis_title="eigenmode indexed by descending eigenvalue order",
-        yaxis_range=[720, 800]
+        yaxis_range=[0, 800]
     )
     fig.show()
-
-    print("Initializing plane wave set...")
-    wavelength = 299792458 / 1e9  # [m]
-    k_wave = 2 * np.pi / wavelength
-    pw_set = PlaneWaveSet(
-        k_wave=k_wave,
-        sampling_rate=14 / wavelength,
-        window=120,
-    )
 
     matlab_structure = "structureC"
 
     print("Load Transfer-matrix from file...")
-    with open("data/transfer_mat_%s.pkl" % matlab_structure, "rb") as file:
+    with open("data/transfer_mat_%s_from_%s.pkl" % (matlab_structure, pw_set_model), "rb") as file:
         transfer_matrix = pickle.load(file)
 
     # Eigen values and vectors of the transfer matrix
@@ -64,6 +55,6 @@ if __name__ == '__main__':
         xaxis_title="$|~\lambda~|$",
         xaxis_range=[0, 1.05],
         yaxis_title="eigenmode indices (descending eigenvalue order)",
-        yaxis_range=[720, 800]
+        yaxis_range=[0, 800]
     )
     fig.show()
